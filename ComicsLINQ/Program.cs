5b621d913ded872f8f8e1a70d679e4e56fac8aaa
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-11 17:09:48 +0200

    Added Main method to show most expensive of Comics using LINQ query

--- ComicsLINQ/Program.cs
+++ ComicsLINQ/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ComicsLINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ComicsLINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IEnumerable<Comic> mostExpensive =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby -Comic.Prices[comic.Issue]
+                select comic;
+
+            foreach (Comic comic in mostExpensive)
+            {
+                Console.WriteLine($"{comic} jest wart {Comic.Prices[comic.Issue]:c}.");
+            }
         }
     }
 }
